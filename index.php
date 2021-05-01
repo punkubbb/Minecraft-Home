2b0cb7d831be5a0ccd1530459eb67d9b43047d9f
unknown <payperview1123@gmail.com>
2021-05-02 02:43:25 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!-- NuengKuB V0.1 -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MoonLight - Home</title>
+	<meta name="description" content="💖 IP : MoonLight.in.th 🌈 เวอร์ชั่น : 1.8 -1.16.5 ⚡ เซิร์ฟเวอร์ เอาชีวิตรอด มินิเกมส์ เควสเยอะ สาวเยอะ สังคมดี 🎁">
+	<meta property="og:title" content="MoonLight - Home">
+	<meta property="og:site_name" content="MoonLight">
+	<meta property="og:description" content="เซิร์ฟเวอร์เอาชีวิตรอด,minecraft,survival,survival1.8-1.16,survival1.16.5,thailand">
+	<meta property="og:image" content="/img/bg.jpg">
+	<meta property="og:url" content="https://moonlight.in.th/">
+	<link rel = "icon" href = "img/logo.png"
+	type = "image/x-icon">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/nuengkub.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div class="logo">
+			<img src="img/logo.png" alt="MoonLight">
+		</div>
+		<div class="items">
+			<a href="https://discord.gg/HC6ebBt2x7" class="item discord">
+			<div>
+				<img src="img/discord.png" alt="Discord" class="img">
+				<p class="title">Discord</p>
+			</div>
+			</a>
+
+			<a href="http://shop.moonlight.in.th/" class="item store">
+			<div>
+				<img src="img/store.png" alt="Minecraft store icon" class="img">
+				<p class="title">Store</p>
+			</div>
+			</a>
+
+
+		</div>
+
+		<div class="playercount">
+			<p>🌈 มีผู้เล่นกำลังออนไลน์ <span class="sip" data-ip="moonlight.in.th" data-port="25565">
+			0</span> ไอดี 💚 <span class="ip">MoonLight.in.th</span></p>
+		</div>
+	</div>
+	
+	  
+	<script src="https://code.jquery.com/jquery-1.11.2.min.js" type="text/javascript"></script>
+	<script src="js/firefly.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+</body>
+</html>
